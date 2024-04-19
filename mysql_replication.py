5472aab68527f27b1b548272a3076f070a634d5f
Sanod Malla <sanodmalla@Sanods-MacBook-Pro.local>
2024-04-19 16:23:23 +1000

    Upload to s3

--- mysql_replication.py
+++ mysql_replication.py
@@ -2,6 +2,8 @@ from pymysqlreplication import BinLogStreamReader
 from pymysqlreplication import row_event
 import configparser
 import pymysqlreplication
+import csv
+import boto3
 
 #get mysql connection info
 
@@ -27,7 +29,51 @@ b_stream = BinLogStreamReader(
                  row_event.UpdateRowsEvent]
 )
 
-for event in b_stream:
-    event.dump()
+order_events = []
+
+for binlogevent in b_stream:
+    for row in binlogevent.rows:
+        if binlogevent.table == 'orders':
+            event = {}
+            if isinstance(
+                binlogevent,row_event.DeleteRowsEvent
+            ):
+                event["action"] = "delete"
+                event.update(row["values"].items())
+            elif isinstance(
+                binlogevent,row_event.UpdateRowsEvent
+            ):
+                event["action"] = "update"
+                event.update(row["after_values"].items())
+            elif isinstance(
+                binlogevent,row_event.WriteRowsEvent
+            ):
+                event["action"] = "insert"
+                event.update(row["values"].items())
+            order_events.append(event)
 
 b_stream.close()
+
+keys = order_events[0].keys()
+local_filename = 'orders_extract.csv'
+with open(local_filename, 'w', newline='') as output_file:
+    dict_writer = csv.DictWriter(output_file, keys, delimiter='|')
+    dict_writer.writerows(order_events)
+
+#Load aws credential values 
+    
+parser = configparser.ConfigParser()
+parser.read("pipeline.config")
+access_key = parser.get("aws_boto_credentials", "access_key")
+secret_key = parser.get("aws_boto_credentials", "secret_key")
+bucket_name = parser.get("aws_boto_credentials", "bucket_name")
+
+s3 = boto3.client(
+    's3',
+    aws_access_key_id = access_key,
+    aws_secret_access_key = secret_key,  
+)
+
+s3_file = local_filename
+
+s3.upload_file(local_filename, bucket_name, s3_file)
